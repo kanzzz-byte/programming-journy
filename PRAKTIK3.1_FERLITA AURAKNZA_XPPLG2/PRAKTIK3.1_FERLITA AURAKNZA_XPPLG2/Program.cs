b5b798b35ebdea076f119da73d87e34e535bb5f8
kanzzz-byte <aurakanza258751@student.smkn1kandeman.sch.id>
2025-10-21 09:30:56 +0700

    Add files via upload

---
+++ PRAKTIK3.1_FERLITA AURAKNZA_XPPLG2/PRAKTIK3.1_FERLITA AURAKNZA_XPPLG2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRAKTIK3._1_FERLITA_AURAKNZA_XPPLG2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // input angka pertama dan kedua 
+            Console.Write("Masukkan angka pertama:");
+                int angka1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Masukkan angka kedua: ");
+            int angka2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n---- Hasil Operasi Penjumlahan ----");
+
+            // PENJUMLAHAN (+)
+            Console.WriteLine($" {angka1} + {angka2} = {angka1 + angka2} ");
+        }
+    }
+}
---
+++ PRAKTIK3.1_FERLITA AURAKNZA_XPPLG2/PRAKTIK3.1_FERLITA AURAKNZA_XPPLG2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PRAKTIK3.1_FERLITA AURAKNZA_XPPLG2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PRAKTIK3.1_FERLITA AURAKNZA_XPPLG2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9d8f4bf8-ae8f-4c10-8b32-430ae4a91fdc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
