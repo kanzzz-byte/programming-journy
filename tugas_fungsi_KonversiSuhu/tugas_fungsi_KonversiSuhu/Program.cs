51853ee923d71a5c42deeaacfc6d88c78efd01dd
kanzzz-byte <aurakanza258751@student.smkn1kandeman.sch.id>
2025-10-21 14:18:22 +0700

    Add files via upload

---
+++ tugas_fungsi_KonversiSuhu/tugas_fungsi_KonversiSuhu/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_fungsi_KonversiSuhu
+{
+    internal class Program
+    {
+        // Fungsi KonversiSuhu : mengubah suhu dari Celcius ke Fahrenhiet 
+        static double konversisuhu(double c)
+        {
+            double f = (c * 9 / 5) + 32; 
+            return f;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Program KonversiSuhu Celcius ke Fahrenhiet ===");
+
+            // Input dari pengguna 
+            Console.Write(" Masukkan Suhu dalam Celcius : ");
+            double suhuC = Convert.ToDouble(Console.ReadLine());
+
+            // Panggil fungsi KonversiSuhu 
+            double suhuF = konversisuhu(suhuC);
+
+            // Tampilkan hasil konversi 
+            Console.WriteLine($"{suhuC}°C sama dengan {suhuF}°F");
+
+            Console.WriteLine("=== Program Selesai ==="); 
+        }
+    }
+}
---
+++ tugas_fungsi_kali/tugas_fungsi_kali/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_fungsi_kali
+{
+    internal class Program
+    {
+        // Fungsi Kali: menerima tiga bilangan dan menampilkan hasil perkalian
+        static void kali(double a, double b, double c)
+        {
+            double hasil = a * b * c;
+            Console.WriteLine($"Hasil perkalian dari {a}, {b}, {c} adalah : {hasil}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===== Program Perkalian 3 Angka ====");
+
+            // Meminta input dari pengguna 
+            Console.WriteLine("Masukkan angka pertama : ");
+            double angka1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Masukkan angka kedua : ");
+            double angka2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Masukkan angka ketiga : ");
+            double angka3 = Convert.ToDouble(Console.ReadLine());
+
+            // Memanggil fungsi kali 
+            kali(angka1, angka2, angka3);
+
+            Console.WriteLine("==== Program Selesai ====");
+        }
+    }
+}
