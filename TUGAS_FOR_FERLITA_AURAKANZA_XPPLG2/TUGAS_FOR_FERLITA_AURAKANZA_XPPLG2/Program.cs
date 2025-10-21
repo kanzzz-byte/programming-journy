06a20e1e80752c097f4a7ec6fc0623b70bc835bc
kanzzz-byte <aurakanza258751@student.smkn1kandeman.sch.id>
2025-10-21 10:07:56 +0700

    Add files via upload

---
+++ TUGAS_FOR_FERLITA_AURAKANZA_XPPLG2/TUGAS_FOR_FERLITA_AURAKANZA_XPPLG2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUGAS_FOR_FERLITA_AURAKANZA_XPPLG2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabel Perkalian 5");
+            for (int i = 1; i <= 10; i++)
+            {
+                int hasil = 5 * i;
+                Console.WriteLine($"5 * {i} = {hasil} ");
+            }
+        }
+    }
+}
