762767a31355593b984d24d50a2ff5773755c3e7
kanzzz-byte <aurakanza258751@student.smkn1kandeman.sch.id>
2025-10-21 10:02:01 +0700

    Add files via upload

---
+++ PRAKTK_6.5_FERLITA_AURAKANZA_XPPLG2/PRAKTK_6.5_FERLITA_AURAKANZA_XPPLG2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRAKTK_6._5_FERLITA_AURAKANZA_XPPLG2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Inisalisasi variabel penghitung (counter)
+            int angka = 1;
+
+            Console.WriteLine(" Menampilkan angka 1 sampai 20 : ");
+
+            // 2. Blok 'do-while'
+            do
+            {
+                // Tampilkan nilai variabel 'angka' saat ini 
+                Console.WriteLine(angka);
+
+                // Tampilkan nilai 'angka' sebanyak 1 (increement)
+                angka++;
+                // uji kondisi : Loop berlanjut selama 'angka' kurang dari atau sama dengan 
+            } while (angka <= 20);
+
+
+            Console.WriteLine("Selesai. ");
+
+        }
+    }
+}
---
+++ PRAKTK_6.5_FERLITA_AURAKANZA_XPPLG2/PRAKTK_6.5_FERLITA_AURAKANZA_XPPLG2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PRAKTK_6.5_FERLITA_AURAKANZA_XPPLG2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PRAKTK_6.5_FERLITA_AURAKANZA_XPPLG2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7c2e53fe-238f-45dd-aa81-3d568c8cf0ea")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
