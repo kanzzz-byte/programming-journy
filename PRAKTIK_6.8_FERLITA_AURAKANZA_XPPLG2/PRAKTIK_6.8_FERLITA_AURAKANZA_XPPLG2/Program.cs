3e04a8b52386fe360d9a6b667f213fdcd2c6334a
kanzzz-byte <aurakanza258751@student.smkn1kandeman.sch.id>
2025-10-21 10:07:08 +0700

    Add files via upload

---
+++ PRAKTIK_6.8_FERLITA_AURAKANZA_XPPLG2/PRAKTIK_6.8_FERLITA_AURAKANZA_XPPLG2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRAKTIK_6._8_FERLITA_AURAKANZA_XPPLG2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String ulangiPilihan;
+
+            do
+            {
+                // --- Blok Proses yang Akan Diulang ---
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("Masukkan nama Anda : ");
+                string nama = Console.ReadLine();
+                Console.WriteLine($"Halo, {nama}! Proses telah selesi. ");
+                // -------------------------------------
+                // Pertanyaan untuk Mengulang 
+                Console.WriteLine("Apakah Anda ingin lagi ? (ya/tidak) : ");
+                ulangiPilihan = Console.ReadLine().ToLower(); // Mengambil input dan mengubah ke huruf kecil 
+
+                Console.WriteLine(); // Baris kosong untuk kerapian 
+
+                // Kondisi diperiksa di akhir : Ulangi selama pengguna mengetik "ya"
+            } while (ulangiPilihan == "ya");
+            Console.WriteLine("Terima kasih. Program selesai. ");
+        }
+    }
+}
