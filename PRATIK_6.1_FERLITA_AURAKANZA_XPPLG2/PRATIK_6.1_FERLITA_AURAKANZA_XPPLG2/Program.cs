ca9d71ffc27143748bb43289e1a448cb2775b393
kanzzz-byte <aurakanza258751@student.smkn1kandeman.sch.id>
2025-10-21 09:58:15 +0700

    Add files via upload

---
+++ PRATIK_6.1_FERLITA_AURAKANZA_XPPLG2/PRATIK_6.1_FERLITA_AURAKANZA_XPPLG2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRATIK_6._1_FERLITA_AURAKANZA_XPPLG2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Perulangan untuk menapilkan angka 1 sampai 10 : ");
+
+                // Perulangan for dimulai di sini 
+                for (int i = 1; i <= 10; i++)
+            {
+                // Blok kode yang akan diulang
+                Console.WriteLine(" Angka ke- " + i);
+            }
+
+            Console.WriteLine("\nPerulangan selessai. ");
+        }
+    }
+}
