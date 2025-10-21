96e327d0faf755b4f7e5ecca371cf69079baba8b
kanzzz-byte <aurakanza258751@student.smkn1kandeman.sch.id>
2025-10-21 15:04:27 +0700

    Add files via upload

--- tugas_fungsi_Ratarata/tugas_fungsi_Ratarata/Program.cs
+++ tugas_fungsi_Ratarata/tugas_fungsi_Ratarata/Program.cs
@@ -8,24 +8,25 @@ namespace tugas_fungsi_Ratarata
 {
     internal class Program
     {
+        // Fungsi Ratarata : menerima tiga nilai dan mengembalikan hasil rata-ratanya
         static double HitungRatarata(double nilai1, double nilai2, double nilai3)
         {
-            double hasil = (nilai1 + nilai2 + nilai3) / 3;
+            double hasil = (nilai1 + nilai2 + nilai3 ) / 3;
             return hasil;
         }
         static void Main(string[] args)
         {
-            // Meminta input dari pengguna 
-            Console.WriteLine("Masukkan nilai pertama : ");
+            // Meminta input dari pengguna
+            Console.Write("Masukkan nilai pertama : ");
             double nilai1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Masukkan nilai kedua   : ");
+            Console.Write("Masukkan nilai kedua : ");
             double nilai2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Masukkan nilai ketiga  : ");
+            Console.Write("Masukkan nilai ketiga : ");
             double nilai3 = Convert.ToDouble(Console.ReadLine());
 
-            // Memanggil fungsi dan menampilkan hasilnya
+            // Mwmanggil fungsi dan menapilkan hasilnya 
             double hasil = HitungRatarata(nilai1, nilai2, nilai3);
         }
     }
