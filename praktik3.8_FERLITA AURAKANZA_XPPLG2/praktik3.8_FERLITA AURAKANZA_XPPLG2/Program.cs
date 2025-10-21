b3fdeb7c1613dfe54b17bc2c3984c0e57b6d13d4
kanzzz-byte <aurakanza258751@student.smkn1kandeman.sch.id>
2025-10-21 09:27:54 +0700

    Add files via upload

---
+++ praktik3.8_FERLITA AURAKANZA_XPPLG2/praktik3.8_FERLITA AURAKANZA_XPPLG2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik3._8_FERLITA_AURAKANZA_XPPLG2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Contoh Operator Logika ===");
+
+            Console.Write("Masukkan angka pertama : ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Masukkan angka kedua : ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n---- Hasil Operator Logika ---");
+            Console.WriteLine($"{a} > 5 && {b} < 10 : " + (a > 5 && b < 10 ));
+            Console.WriteLine($"{a} > 5 || {b} < : " + (a > 5 || b < 10));
+            Console.WriteLine($"!({a} > {b}) : " + (!(a > b)));
+
+        }
+    }
+}
