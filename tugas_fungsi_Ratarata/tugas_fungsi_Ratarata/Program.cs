9fd5bc969d0efc41e24813629503916ae9485e53
kanzzz-byte <aurakanza258751@student.smkn1kandeman.sch.id>
2025-10-21 14:19:26 +0700

    Add files via upload

---
+++ tugas_fungsi_Ratarata/tugas_fungsi_Ratarata/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_fungsi_Ratarata
+{
+    internal class Program
+    {
+        static double HitungRatarata(double nilai1, double nilai2, double nilai3)
+        {
+            double hasil = (nilai1 + nilai2 + nilai3) / 3;
+            return hasil;
+        }
+        static void Main(string[] args)
+        {
+            // Meminta input dari pengguna 
+            Console.WriteLine("Masukkan nilai pertama : ");
+            double nilai1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Masukkan nilai kedua   : ");
+            double nilai2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Masukkan nilai ketiga  : ");
+            double nilai3 = Convert.ToDouble(Console.ReadLine());
+
+            // Memanggil fungsi dan menampilkan hasilnya
+            double hasil = HitungRatarata(nilai1, nilai2, nilai3);
+        }
+    }
+}
