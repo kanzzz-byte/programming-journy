1dd46a2bbce338622cacc140bde7e59420d6aabc
kanzzz-byte <aurakanza258751@student.smkn1kandeman.sch.id>
2025-10-21 10:08:32 +0700

    Add files via upload

---
+++ TUGAS_WHILE_FERLITA_AURAKANZA_XPPLG2/TUGAS_WHILE_FERLITA_AURAKANZA_XPPLG2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUGAS_WHILE_FERLITA_AURAKANZA_XPPLG2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int total = 0;
+
+            while (i <= 5)
+                {
+                total += i;
+                i++;
+            }
+            Console.WriteLine($"Total = {total} ");
+        }
+    }
+}
